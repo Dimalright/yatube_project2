aed791d88b45ae79e36a1e74969799c062695964
Стас Басов <stas.basov@whatever.com>
2024-07-09 20:24:09 +0300

    q add new view and url

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse('Main page')
+
+
+def group_posts(request, slug):
+    try:
+        int(slug)
+        return HttpResponse('Ты указал номер группу')
+    except ValueError:
+        return HttpResponse('Любая дичь текстовая')
