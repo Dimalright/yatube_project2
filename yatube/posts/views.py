8bcf887edcbe90ef60cf18c3773190258c9a669b
Стас Басов <stas.basov@whatever.com>
2024-07-28 23:05:56 +0300

    header fixed

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post, Group
+from .models import Post, Group, User
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 
@@ -30,3 +30,25 @@ def group_posts(request, slug):
         'page_obj': page_obj,
     }
     return render(request, 'posts/group_list.html', context)
+
+
+def profile(request, username):
+    user = get_object_or_404(User, username=username)
+    post_list = user.posts.all().order_by('-pub_date')
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'profile_user': user,
+        'page_obj': page_obj,
+    }
+    return render(request, 'posts/profile.html', context)
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    # Здесь код запроса к модели и создание словаря контекста
+    context = {
+        'post': post,
+    }
+    return render(request, 'posts/post_detail.html', context)
